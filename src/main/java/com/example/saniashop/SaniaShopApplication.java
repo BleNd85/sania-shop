f5f8524212e075042fc7d81f8f783121f8afd369
Vladyslav Udod <volodkatraktorist@gmail.com>
2025-02-06 13:08:54 +0200

    init

---
+++ src/main/java/com/example/saniashop/SaniaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.saniashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaniaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaniaShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/saniashop/SaniashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saniashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaniashopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
